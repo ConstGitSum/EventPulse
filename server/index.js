05664d4ff3bb68994d9334dbc4c89ac9ec72e706
Kenneth Torng <ktorng@gmail.com>
2016-07-27 17:27:45 -0500

    More restructuring

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
+const path = require('path');
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackConfig = require('./webpack.config.js');
+const webpackConfig = require('../webpack.config.js');
 
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+var assetFolder = path.join(__dirname, '..', 'client','public');
+
+app.use(express.static(assetFolder));
 app.use(webpackDevMiddleware(webpack(webpackConfig)));
 app.use(bodyParser.json());
 
+// Wild card route for client side routing.
+app.get('/*', function(req, res){
+  res.sendFile( assetFolder + '/index.html' );
+})
 
 app.listen(3000);
