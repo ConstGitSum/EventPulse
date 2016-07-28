b611d34ca7e642759e1c3793ea2e566d0013b184
Kenneth Torng <ktorng@gmail.com>
2016-07-27 21:11:55 -0500

    (test) Install pg, knex, and create knexfile

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/event_pulse_test',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/event_pulse',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production'
+    }
+  }
+};
