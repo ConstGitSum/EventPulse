875ef946dd7d54740dce647e0c23bc28b19c68f3
Jad Carson <Jad.Carson@gmail.com>
2016-07-28 17:31:55 -0500

    (refactor) added some semicolons

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // *** define routes for each model here in index *** //
 router.use('/events', require('./events'));
-router.use('/passportFacebook',require('./passport_facebook'))
-router.use('/auth',require('./authentication'))
+router.use('/passportFacebook',require('./passport_facebook'));
+router.use('/auth',require('./authentication'));
 
 module.exports = router;
