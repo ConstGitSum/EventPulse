ed0e8ddb00be5f28a6d218e7594126fb0af2ef3f
Jad Carson <Jad.Carson@gmail.com>
2016-07-28 11:48:43 -0500

    (bug) added knex variable in knex.js file

--- server/db/knex.js
+++ server/db/knex.js
@@ -1,7 +1,8 @@
 var environment = process.env.NODE_ENV || 'development';
 var config = require('../knexfile.js')[environment];
 
-module.exports = require('knex')(config);
+knex = require('knex')(config);
 
 knex.migrate.latest([config]);
 knex.seed.run([config]);
+module.exports = knex
