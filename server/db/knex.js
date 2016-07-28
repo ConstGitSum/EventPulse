e3dd4692504ed2170ce3d72d9f231052db85b6c4
Jad Carson <Jad.Carson@gmail.com>
2016-07-28 11:56:50 -0500

    (bug) added semicolon and var

--- server/db/knex.js
+++ server/db/knex.js
@@ -1,8 +1,8 @@
 var environment = process.env.NODE_ENV || 'development';
 var config = require('../knexfile.js')[environment];
 
-knex = require('knex')(config);
+var knex = require('knex')(config);
 
 knex.migrate.latest([config]);
 knex.seed.run([config]);
-module.exports = knex
+module.exports = knex;
