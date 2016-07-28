28c5f86704e0a9b73cee19af025eac1d32ada7f8
Kenneth Torng <ktorng@gmail.com>
2016-07-28 09:26:27 -0500

    (test) Move events api route out of index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var Event = require('../models/event');
-
-
-// *** GET all events *** //
-router.get('/events', function(req, res, next) {
-  Event.getAll()
-    .then((events) => {
-      res.status(200).json(events);
-    })
-    .catch((err) => {
-      next(error);
-    });
-});
-
+// *** define routes for each model here in index *** //
+router.use('/events', require('./events'));
 
 module.exports = router;
