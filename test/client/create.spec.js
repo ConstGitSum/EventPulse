687594875bb317f30e6959163c77c8768b431beb
sxcw <yuan329419@gmail.com>
2016-07-30 10:46:24 -0500

    (test) Add inputs tests

--- test/client/create.spec.js
+++ test/client/create.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { renderIntoDocument, scryRenderedDOMComponentsWithTag } from 'react-addons-test-utils';
+import { renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate } from 'react-addons-test-utils';
 import { expect } from 'chai';
 
 import {Create} from '../../client/components/Create';
@@ -13,7 +13,6 @@ describe('Create', () => {
     it('should display labels', () => {
       const component = renderIntoDocument(<Create />)
       const label = scryRenderedDOMComponentsWithTag(component, 'label')
-      console.log('label~~~~',label)
       expect(label.length).to.equal(9)
       expect(label[0].textContent).to.equal('Event Title')
       expect(label[1].textContent).to.equal('Description')
@@ -24,6 +23,23 @@ describe('Create', () => {
       expect(label[6].textContent).to.equal('Number of Guests')
       expect(label[7].textContent).to.equal('Privacy')
       expect(label[8].textContent).to.equal('Visibility')
+    })
+  })
+
+  describe('Display Form inputs', () => {
+    it('should display inputs', () => {
+      const component = renderIntoDocument(<Create />)
+      const inputs = scryRenderedDOMComponentsWithTag(component, 'inputs')
+      expect(inputs.length).to.equal(9)
+      expect(inputs[0].textContent).to.equal('Event Title')
+      expect(inputs[1].textContent).to.equal('Description')
+      expect(inputs[2].textContent).to.equal('Location')
+      expect(inputs[3].textContent).to.equal('Date')
+      expect(inputs[4].textContent).to.equal('Start Time')
+      expect(inputs[5].textContent).to.equal('End Time')
+      expect(inputs[6].textContent).to.equal('Number of Guests')
+      expect(inputs[7].textContent).to.equal('Privacy')
+      expect(inputs[8].textContent).to.equal('Visibility')
     })
   })
 
