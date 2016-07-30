0688437087deae61268aa5591828c1333325f6a0
Kenneth Torng <ktorng@gmail.com>
2016-07-30 13:53:49 -0500

    (feat) Implement react-router

--- client/components/EventCreate.js
+++ client/components/EventCreate.js
@@ -1,1 +1,10 @@
+import React from 'react';
+
 // EventCreate component
+export default class EventCreate extends React.Component {
+  render() {
+    return (
+      <div>Event Create</div>
+    );
+  }
+}
--- client/components/EventDetails.js
+++ client/components/EventDetails.js
@@ -1,1 +1,10 @@
-// EventDetails component
+import React from 'react';
+
+export default class EventDetails extends React.Component {
+  render() {
+    return (
+      <div>Event Details for event {this.props.params.eventId}</div>
+    );
+  }
+}
+
