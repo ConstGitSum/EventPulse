49f8bffd294e180ffd14727a87988509b106cf25
davidlewallen <lewallen.david@gmail.com>
2016-07-30 11:51:54 -0500

    (refactor) Code clean up. seedData and component accessable throughout EventDetails test

--- test/client/eventDetails.spec.js
+++ test/client/eventDetails.spec.js
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom';
 import { renderIntoDocument, scryRenderedDOMComponentsWithTag } from 'react-addons-test-utils';
 import { expect } from 'chai';
 
-import EventDetails from '../../client/components/EventDetails'
+import { EventDetails } from '../../client/components/EventDetails'
 
 var chai = require('chai');
 
 describe('EventDetails', () => {
 
+  const seedData = { title: "TDD Test Title",
+                     description: "TDD Test Description",
+                     location: "TDD Test Location",
+                     time: "TDD Test Time"
+                   }
+  const component = renderIntoDocument(<EventDetails currentEvent={seedData} />)
+
   describe('Display data', () => {
     it('should display data', () => {
-      const seedData = { title: "TDD Test Title",
-                         description: "TDD Test Description",
-                         location: "TDD Test Location",
-                         time: "TDD Test Time"
-                       }
-      const component = renderIntoDocument(<EventDetails event={seedData} />)
       const paragraph = scryRenderedDOMComponentsWithTag(component, 'p')
       expect(paragraph.length).to.equal(4)
       expect(paragraph[0].textContent).to.equal('Title: TDD Test Title')
@@ -28,12 +29,6 @@ describe('EventDetails', () => {
 
   describe('Display Buttons', () => {
     it('should display a Join, Hide and Chat buttons', () => {
-      const seedData = { title: "TDD Test Title",
-                         description: "TDD Test Description",
-                         location: "TDD Test Location",
-                         time: "TDD Test Time"
-                       }
-      const component = renderIntoDocument(<EventDetails event={seedData} />)
       const buttons = scryRenderedDOMComponentsWithTag(component, 'button')
       expect(buttons.length).to.equal(3);
       expect(buttons[0].textContent).to.equal('Join')
