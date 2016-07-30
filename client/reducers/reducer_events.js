250b81cede975d159b4481054699cc6dd64968ef
davidlewallen <lewallen.david@gmail.com>
2016-07-30 17:03:10 -0500

    (refactor) Refactor code to use the currentEvent

--- client/reducers/reducer_events.js
+++ client/reducers/reducer_events.js
@@ -6,8 +6,8 @@ export default function(state = {}, action) {
       // action.payload is a promise that resolved with the new user object that was
       // returned after the server API call to join event
       const newState = Object.assign({}, state)
-      newState.guest = [...newState.guest, action.payload.data]
-      
+      newState.currentEvent.guests = [...newState.currentEvent.guests, action.payload.data]
+
       return newState
   }
 }
