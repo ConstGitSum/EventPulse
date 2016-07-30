c42f7cac5aacf2ec36e6be40fa02d6c083053412
davidlewallen <lewallen.david@gmail.com>
2016-07-29 19:49:15 -0500

    (feat) Created beginner of EventDetails. Able to pass state around

--- client/components/EventDetails.js
+++ client/components/EventDetails.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'react-redux'
 
-export default class EventDetails extends React.Component {
+class EventDetails extends Component {
   render() {
     return (
-      <div>Event Details for event {this.props.params.eventId}</div>
-    );
+      <div className="event-details">
+        <h1>Pulse</h1>
+
+        <div className="btn-group btn-primary" role="group">
+          <button
+            type="button" 
+            className="btn btn-primary">Join</button>
+          <button 
+            type="button" 
+            className="btn btn-danger">Hide</button>
+        </div>
+
+        <div>
+          <span className="details">Details:</span>
+          <span className="attendance">#/##</span>
+        </div>
+
+        <div>
+          <p>Title: {this.props.currentEvent.title}</p>
+          <p>Description: {this.props.currentEvent.description}</p>
+          <p>Location: {this.props.currentEvent.location}</p>
+          <p>Time: {this.props.currentEvent.time}</p>
+        </div>
+
+        <button className="btn btn-primary">Chat</button>
+      </div>
+    )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    currentEvent: state.currentEvent
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    joinEvent: joinEvent
+  }, dispatch)
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventDetails)
