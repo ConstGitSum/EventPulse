a0a88d20ce9e6ca1727d1db60552c360e3845486
Kenneth Torng <ktorng@gmail.com>
2016-07-30 18:25:15 -0500

    (test) Test Home component log states

--- test/client/home.spec.js
+++ test/client/home.spec.js
@@ -2,45 +2,47 @@ process.env.NODE_ENV = 'test';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 import { renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate } from 'react-addons-test-utils';
 import { expect } from 'chai';
 import axios from 'axios';
 
 import { Home } from '../../client/components/Home';
 
+const mockStore = configureStore([])({});
 
-describe('Home Auth Component', () => {
+describe('Home Component', () => {
 
-  xit('renders a button and a link when logged out', () => {
+  it('should render a facebook login link when logged out', () => {
     let logState = false;
     const fetchLogState = () => logState = false;
     const component = renderIntoDocument(
-      <Home 
-        pulse={1} 
-        logState={logState}
-        fetchLogState={fetchLogState} />
+      <Provider store={mockStore}>
+        <Home 
+          pulse={1} 
+          logState={logState}
+          fetchLogState={fetchLogState} />
+      </Provider>
     );
-    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
     const links = scryRenderedDOMComponentsWithTag(component, 'a');
-    expect(buttons.length).to.equal(1);
-    expect(links.length).to.equal(1);
-    expect(buttons[0].textContent).to.equal('Pulse it');
-    expect(links[0].textContent).to.equal('facebook!');
 
+    expect(links.length).to.equal(1);
+    expect(links[0].textContent).to.equal('facebook!');
   })
 
-  xit('renders 2 buttons when logged in',() => {
+  it.only('should render a logout button when logged in',() => {
     let logState = true;
     const fetchLogState = () => logState = true;
     const component = renderIntoDocument(
-      <Home 
-        pulse={1} 
-        logState={logState}
-        fetchLogState={fetchLogState} />
+      <Provider store={mockStore}>
+        <Home 
+          pulse={1} 
+          logState={logState}
+          fetchLogState={fetchLogState} />
+      </Provider>
     );
-    console.log(component)
     const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
-    console.log(buttons)
     const links = scryRenderedDOMComponentsWithTag(component, 'a');
     expect(buttons.length).to.equal(2);
     expect(links.length).to.equal(0);
