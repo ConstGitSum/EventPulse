f3cb0ed313b2b7fa543de2bebddd6692353fa200
sxcw <yuan329419@gmail.com>
2016-07-31 12:33:59 -0500

    (bug) Fix unknown props warnings

--- client/components/Create.js
+++ client/components/Create.js
@@ -1,94 +1,69 @@
-import React, {Component,PropTypes} from 'react';
-import {reduxForm} from 'redux-form';
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 import {createEvent} from '../actions/actions';
 
-export const fields=['title','description','location','date','startTime','endTime','guests','privacy','visibility'];
-
-export class Create extends Component {
-  // const {handleSubmit} = this.props;
-  // const handleSubmit = this.props.handleSubmit;
-  // redux form is injecting helpers into this.props, so we can get to the helpers by saying this.props (this.props.handleSubmit, this.props.fields)
-  // map inputs to fields, 
-  //const {fields:{title,description,location,startTime,endTime,guests,privacy,visibility}, handleSubmit} = this.props;
-  
-  // const title = this.props.fields.title
-  // {...title} destructuring ==> formProps = {title}
-  // when handleSubmit is called, an object with key title,value title input will go to props of createEvent
-  render() {   
-    const {
-      fields:{title,description,location,date,startTime,endTime,guests,privacy,visibility},
-        handleSubmit,
-        submitting
-      } = this.props;
-
-    return (
-      <form onSubmit={this.props.handleSubmit}>
-        <h3>Create New Event</h3>
-        <div className={`form-group ${title.touched && title.invalid ? 'has-danger': null}`}>
-          <label>Event Title</label>
-          <input type='text' placeholder='Movie night' className='form-control' {...title.input}/>
-          <div className='text-help'>
-            {/*title.touched ? title.error: ''*/}
-          </div>
-        </div>
-        <div className='form-group'>
-          <label>Description</label>
-          <textarea placeholder='Some description...' className='form-control' {...description.input}/>
-        </div>
-        <div className='form-group'>
-          <label>Location</label>
-          <input type='text' placeholder='701 Brazos Street, Austin, TX' className='form-control' {...location.input}/>
-        </div>
-        <div className='form-group'>
-          <label>Date</label>
-          <input type='date' className='form-control' {...date.input}/>
-        </div>
-        <div className='form-group'>
-          <label>Start Time</label>
-          <input type='time' className='form-control' {...startTime.input}/>
-        </div>
-        <div className='form-group'>
-          <label>End Time</label>
-          <input type='time' className='form-control' {...endTime.input}/>
-        </div>
-        <div className='form-group'>
-          <label>Number of Guests</label>
-          <input type='number' className='form-control' {...guests.input}/>
-        </div>
-        <div className='form-group'>
-          <label>Privacy</label>
-          <input type='checkbox' className='form-control' {...privacy.input}/>
-        </div>
-        <div className='form-group'>
-          <label>Visibility</label>
-          <input type='checkbox' className='form-control' {...visibility.input}/>
-        </div>
-        <button type='submit' disabled={submitting} className='btn btn-primary'> {submitting ? <i/> : <i/>} Create Event</button>
-      </form>
-    );
+const validate = values => {
+  const errors = {}
+  if (!values.title) {
+    errors.title = 'Required'
+  } else if (values.title.length > 15) {
+    errors.title = 'Must be 15 characters or less'
   }
+  if (!values.description) {
+    errors.description = 'Required'
+  } else if (values.description.length > 205) {
+    errors.description = 'Must be 205 characters or less'
+  }
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+  if (!values.age) {
+    errors.age = 'Required'
+  } else if (isNaN(Number(values.age))) {
+    errors.age = 'Must be a number'
+  } else if (Number(values.age) < 18) {
+    errors.age = 'Sorry, you must be at least 18 years old'
+  }
+  return errors
 }
 
-function validate(values){
-  const errors={};
-  if(!values.title){
-    errors.title='Enter an event title';
-  }
-  if(!values.description){
-    errors.title='Enter an event description';
-  }
-  return errors;
-}
+const renderField = props => (
+  <div>
+    <label>{props.placeholder}</label>
+    <div>
+      <input {...props.input}/>
+      {props.touched && props.error && <span>{props.error}</span>}
+    </div>
+  </div>
+)
 
-Create.propTypes = {
-  fields: PropTypes.object.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  submitting: PropTypes.bool.isRequired
+const Create = (props) => {
+  const { handleSubmit, pristine, reset, submitting } = props
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Create New Event</h3>
+      <Field name="title" type="text" component={renderField} placeholder="Event title"/>
+      <Field name="description" type="text" component={renderField} placeholder="Description"/>
+      <Field name="location" type="text" component={renderField} placeholder="Location"/>
+      <Field name="date" type="date" component={renderField} placeholder="Date"/>
+      <Field name="time" type="time" component={renderField} placeholder="Time"/>
+      <Field name="duration" type="time" component={renderField} placeholder="Duration"/>
+      <Field name="guests" type="number" component={renderField} placeholder="Number of guests"/>
+      <label>Privacy</label>
+        <Field name="privacy" type="checkbox" component={renderField}/>
+      <label>Visibility</label>
+      <Field name="visibility" type="checkbox" component={renderField}/>
+      <div>
+        <button type="submit" className='btn btn-primary' disabled={submitting}>Create Event</button>
+        <button type="button" className='btn btn-primary' disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+      </div>
+    </form>
+  )
 }
 
 export default reduxForm({
-  form: 'CreateEventForm',
-  fields,
-  validate
-}, null, {onSubmit: createEvent})(Create);
-
+  form: 'createEventForm',  // a unique identifier for this form
+  validate  
+}, null, {onSubmit: createEvent})(Create)
