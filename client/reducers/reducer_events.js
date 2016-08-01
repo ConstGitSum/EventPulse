850227ae76f7d64879dcaffe954e611eff6c10b7
davidlewallen <lewallen.david@gmail.com>
2016-08-01 15:30:00 -0500

    (feat) Add handler for HIDE_EVENT

--- client/reducers/reducer_events.js
+++ client/reducers/reducer_events.js
@@ -1,13 +1,32 @@
-import { JOIN_EVENT } from '../actions/actions'
+import { JOIN_EVENT, HIDE_EVENT } from '../actions/actions'
 
+/**
+ * Reducer to update state based on EventDetails' actions
+ * @param  {Object} state  Current store's state
+ * @param  {Object} action 
+ * @return {Object}        New state
+ */
 export default function(state = {}, action) {
+  let newState;
+
   switch (action.type) {
+    /**
+     * Join the current event
+     * @type {Object}
+     */
     case JOIN_EVENT:
       // action.payload is a promise that resolved with the new user object that was
       // returned after the server API call to join event
-      const newState = Object.assign({}, state)
+      newState = Object.assign({}, state)
       newState.currentEvent.guests = [...newState.currentEvent.guests, action.payload.data]
-
+      return newState
+    /**
+     * Hide the current event
+     * @type {Object}
+     */
+    case HIDE_EVENT:
+      newState = Object.assign({}, state)
+      newState.currentEvent = {}
       return newState
   }
 }
