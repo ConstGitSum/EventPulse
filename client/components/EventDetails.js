af8f6e7faf9dfc7aefa17233171ea3ab8957fe70
David Lewallen <lewallen.david@gmail.com>
2016-08-01 19:04:03 -0500

    feat20 (#35)
    * (feat) Implement joinEvent action and cleaned up code
    
    * (feat) Created beginner of EventDetails. Able to pass state around
    
    * (test) Add test for EventDetails.js
    
    * (feat) Add reducer for all events. Currently will only handle JOIN_EVENT from EventDetails.js
    
    * (feat) Add ReduxPromise as middleware and code clean up
    
    * (refactor) Code clean up. seedData and component accessable throughout EventDetails test
    
    * (refactor) Clean up code
    
    * (feat) Add join button handling and cleaned up code
    
    * (refactor) Refactor code to use the currentEvent
    
    * (test) Implement reducer test for JOIN_EVENT
    
    * (feat) Add action for hiding an event
    
    * (feat) Add joinEvent and hideEvent functions and action creators
    
    * (refactor) Updated joinEvent endpoint
    
    * (feat) Add handler for HIDE_EVENT
    
    * (test) Add test cases for HIDE_EVENT and code clean up
    
    * (refactor) Renamed for continuity
    
    * (refactor) Rename for continuity
    
    * (refactor) Code clean up
    
    * (refactor) Code clean up
    
    * (refactor) Code clean up
    
    * (feat) Add eventDetails reducer to combineReducers
    
    * (refactor) Code clean up
    
    * (refactor) Code clean up
    
    * (refactor) Code clean up
    
    * (refactor) Code clean up
    
    * (refactor) Code clean up
    
    * (refactor) Kenny broke the codes
    
    * (refactor) Move reducer tests to reducer.spec.js

--- client/components/EventDetails.js
+++ client/components/EventDetails.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { browserHistory } from 'react-router';
 
-export default class EventDetails extends React.Component {
+import { joinEvent, hideEvent } from '../actions/actions';
+
+export class EventDetails extends Component {
+  /**
+   * Join the current event by the current user
+   * @return {undefined} 
+   */
+  onEventJoin() {
+    this.props.joinEvent(this.props.currentEvent.eventId, this.props.currentUser.userId);
+  }
+
+  /**
+   * Hide the event from the current user
+   * @return {undefined}
+   */
+  onEventHide() {
+    this.props.hideEvent(this.props.currentEvent.eventId, this.props.currentUser.userId);
+    browserHistory.push('/EventList');
+  }
+
   render() {
     return (
-      <div>Event Details for event {this.props.params.eventId}</div>
-    );
+      <div className="event-details">
+        <h1>Pulse</h1>
+
+        <div className="btn-group btn-primary" role="group">
+          <button
+            onClick={this.onEventJoin}
+            type="button" 
+            className="btn btn-primary">Join</button>
+          <button 
+            onClick={this.onEventHide}
+            type="button" 
+            className="btn btn-danger">Hide</button>
+        </div>
+
+        <div>
+          <span className="details">Details:</span>
+          <span className="attendance">#/##</span>
+        </div>
+
+        <div>
+          <p>Title: {this.props.currentEvent.title}</p>
+          <p>Description: {this.props.currentEvent.description}</p>
+          <p>Location: {this.props.currentEvent.location}</p>
+          <p>Time: {this.props.currentEvent.time}</p>
+        </div>
+        <button className="btn btn-primary">Chat</button>
+      </div>
+    )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    currentEvent: state.currentEvent,
+    currentUser:  state.currentUser,
+    guestList:    state.guestList
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    joinEvent, hideEvent
+  }, dispatch)
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventDetails)
---
+++ test/client/eventDetails.spec.js
@@ -0,0 +1,64 @@
+process.env.NODE_ENV = 'test';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate } from 'react-addons-test-utils';
+import { expect } from 'chai';
+
+import { EventDetails } from '../../client/components/EventDetails'
+import reducer_eventDetails from '../../client/reducers/reducer_eventDetails'
+
+var chai = require('chai');
+
+describe('EventDetails Component', () => {
+  const seedData = { 
+    eventsList: [{
+      id: 1,
+      title: "Pokemongodb party",
+      description: "Catch pokemon and do some coding",
+      location: "701 Brazos St, Austin, TX 78701",
+      time: "2016-08-30T13:00:00.000Z",
+      guests: [],
+    }],
+    currentUser: {
+      id: 1,
+    },
+    currentEvent: {
+      id: 1,
+      title: "Pokemongodb party",
+      description: "Catch pokemon and do some coding",
+      location: "701 Brazos St, Austin, TX 78701",
+      time: "2016-08-30T13:00:00.000Z",
+      guests: [],
+    }
+  } 
+
+  const component = renderIntoDocument(
+    <EventDetails 
+      currentEvent={seedData.currentEvent} 
+      currentUser={seedData.currentUser} />
+  )
+
+  describe('Display data', () => {
+    it('should display data', () => {
+      const paragraph = scryRenderedDOMComponentsWithTag(component, 'p')
+      expect(paragraph.length).to.equal(4)
+      expect(paragraph[0].textContent).to.equal('Title: Pokemongodb party')
+      expect(paragraph[1].textContent).to.equal('Description: Catch pokemon and do some coding')
+      expect(paragraph[2].textContent).to.equal('Location: 701 Brazos St, Austin, TX 78701')
+      expect(paragraph[3].textContent).to.equal('Time: 2016-08-30T13:00:00.000Z')
+    })
+  })
+
+  describe('Buttons', () => {
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button')
+
+    it('should display a Join, Hide and Chat buttons', () => {
+      expect(buttons.length).to.equal(3);
+      expect(buttons[0].textContent).to.equal('Join')
+      expect(buttons[1].textContent).to.equal('Hide')
+      expect(buttons[2].textContent).to.equal('Chat')
+    })
+  })
+
+})
