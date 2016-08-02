1e47b6b1dc0550088a951ffc66f5e5c38191f4e9
Tony Kung <lps.tony@gmail.com>
2016-08-01 19:08:50 -0500

    change indentation to spaces:2

--- client/components/List.js
+++ client/components/List.js
@@ -4,42 +4,42 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 export default class Lists extends React.Component {
-	constructor(props){
-		super(props); 
-		this.state = {
-			events: [],
-			txt: ''
-		}
-	}
+  constructor(props){
+    super(props); 
+    this.state = {
+      events: [],
+      txt: ''
+    }
+  }
 
-	componentDidMount(){
-		axios.get('/api/events').then((eventData) => {
-			console.log('~~eventData~~~', eventData.data);
-			this.setState({events: eventData.data})
-    	})
-	}
-	
-	handleCreate(){
-		this.setState({txt: '~~~Need a router~~~'});
-	}
+  componentDidMount(){
+    axios.get('/api/events').then((eventData) => {
+      console.log('~~eventData~~~', eventData.data);
+      this.setState({events: eventData.data})
+      })
+  }
+  
+  handleCreate(){
+    this.setState({txt: '~~~Need a router~~~'});
+  }
 
-	render(){
-		return (
-			<div className='event_items'>
-				<h5>Events Happening!</h5>
-					{this.state.events.map((event, index) => {
-						return ( 
-						<ul key={index} className="events">
-							<li>{"What's happening " + event.title}</li>
-							<li>{"Where? " + event.location}</li>
-							<li>{"What are we goin to do? "+ event.description}</li>
-						</ul>
-						)
-					})}
-				<button onClick={this.handleCreate.bind(this)} className='create_event'>Create</button>
-				<span>{this.state.txt}</span>
-			</div>
-		)
-	}
+  render(){
+    return (
+      <div className='event_items'>
+        <h5>Events Happening!</h5>
+          {this.state.events.map((event, index) => {
+            return ( 
+            <ul key={index} className="events">
+              <li>{"What's happening " + event.title}</li>
+              <li>{"Where? " + event.location}</li>
+              <li>{"What are we goin to do? "+ event.description}</li>
+            </ul>
+            )
+          })}
+        <button onClick={this.handleCreate.bind(this)} className='create_event'>Create</button>
+        <span>{this.state.txt}</span>
+      </div>
+    )
+  }
 }
 
--- test/client/list.spec.js
+++ test/client/list.spec.js
@@ -13,32 +13,32 @@ import List from '../../client/components/List';
 let wrapper; 
 
 describe('List component', () => {
-	it('renders as a <div>', () => {
-		wrapper = shallow(<List />);
-		expect(wrapper.type()).to.eql('div');
-	});
+  it('renders as a <div>', () => {
+    wrapper = shallow(<List />);
+    expect(wrapper.type()).to.eql('div');
+  });
 
-	it('Renders the root `div` with the right class', () => {
-	    wrapper = shallow(<List />);
-	    expect(wrapper.find('.event_items')).to.have.length(1);
-  	});
+  it('Renders the root `div` with the right class', () => {
+      wrapper = shallow(<List />);
+      expect(wrapper.find('.event_items')).to.have.length(1);
+    });
 
-	it('Calls componentDidMount lifecycle method', () => {
-	    sinon.spy(List.prototype, 'componentDidMount');
-	    wrapper = mount(<List />);
-	    expect(List.prototype.componentDidMount.calledOnce).to.equal(true);
-  	});	
+  it('Calls componentDidMount lifecycle method', () => {
+      sinon.spy(List.prototype, 'componentDidMount');
+      wrapper = mount(<List />);
+      expect(List.prototype.componentDidMount.calledOnce).to.equal(true);
+    }); 
 
-  	it('Correctly updates the state after axios call in `componentDidMount` was made', () => {
-		axios.get('/api/events').then((eventData) => {
-			this.setState({events: eventData.data})
-    	})
-	    wrapper = mount(<List />);
-	    setTimeout(function() {
-	      expect(wrapper.state().events).to.be.instanceof(Array);
-	      expect(wrapper.state().events.length).to.equal(1);
-	      expect(wrapper.state().events[0].title).to.equal('Pokemongodb party');
-	      expect(wrapper.state().events[0].location).to.equal('701 Brazos St, Austin, TX 78701');
-	    }, 0);
+    it('Correctly updates the state after axios call in `componentDidMount` was made', () => {
+    axios.get('/api/events').then((eventData) => {
+      this.setState({events: eventData.data})
+      })
+      wrapper = mount(<List />);
+      setTimeout(function() {
+        expect(wrapper.state().events).to.be.instanceof(Array);
+        expect(wrapper.state().events.length).to.equal(1);
+        expect(wrapper.state().events[0].title).to.equal('Pokemongodb party');
+        expect(wrapper.state().events[0].location).to.equal('701 Brazos St, Austin, TX 78701');
+      }, 0);
   });
 });
