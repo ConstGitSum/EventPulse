2e4dfbe12c3ed8cf87f10d29a42ada5f78d0a7ce
davidlewallen <lewallen.david@gmail.com>
2016-08-03 17:23:58 -0500

    (refactor) Issues with push, now fixed

--- client/components/EventCreate.js
+++ client/components/EventCreate.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { createEvent, setCurrentEvent } from '../actions/actions';
-import { getCoords } from './helpers';
+import { getCoords } from '../utils/helpers';
 
 export class EventCreate extends Component {
 
