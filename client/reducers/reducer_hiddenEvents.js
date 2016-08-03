4a054deaf043ec8565e680c205151373bbb4c988
davidlewallen <lewallen.david@gmail.com>
2016-08-03 16:13:49 -0500

    (feat) Reducer to handle HIDE EVENT

--- client/reducers/reducer_hiddenEvents.js
+++ client/reducers/reducer_hiddenEvents.js
@@ -8,9 +8,7 @@ export default function(state = [], action) {
      * Handles HIDE_EVENT action by changing state to include the current hidden event
      */
     case HIDE_EVENT:
-      let newState = state
-      newState.hiddenEvents = [...newState.hiddenEvents, action.payload.data.event_id]
-      
+      return [...state, action.payload.data.event_id]
     default:
       return state;
   }
