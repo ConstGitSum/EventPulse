7d1ca450c0106ac861ba1f156c514810f1203331
Kenneth Torng <ktorng@gmail.com>
2016-08-02 23:50:33 -0500

    (feat) Get coords upon creating event

--- server/db/seeds/development/pulse_seed.js
+++ server/db/seeds/development/pulse_seed.js
@@ -45,6 +45,8 @@ exports.seed = function(knex, Promise) {
         description: 'Catch pokemon and do some coding',
         created_by: 1,
         location: '701 Brazos St, Austin, TX 78701',
+        latitude: 30.2688755, 
+        longitude: -97.7405641,
         time: '2016-08-30T08:00:00.000',
         duration: 10800,
         privacy: false
@@ -56,6 +58,8 @@ exports.seed = function(knex, Promise) {
         description: 'A friendly game of basketball',
         created_by: 2,
         location: '2100 Alamo St, Austin, TX 78722',
+        latitude: 30.2770933, 
+        longitude: -97.7185537,
         time: '2016-08-30T10:00:00.000',
         max_guests: 10,
         duration: 3600,
