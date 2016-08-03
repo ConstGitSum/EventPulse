837486c3c0b82952bead69394a6a69a92ab975c0
Chuwei <yuan329419@gmail.com>
2016-08-02 22:00:04 -0500

    Feat45 (#52)
    * (feat) Create redux structure for form
    
    * (feat)Add actions and reducers
    
    * (feat) Connect with setCurrentEvent action creator
    
    * (feat) Fix radio buttons and clean up code
    
    * (feat) Add guests array and clean up code
    
    * (refactor) Code cleanup

---
+++ client/reducers/reducer_eventCreate.js
@@ -0,0 +1,11 @@
+import { CREATE_EVENT } from '../actions/actions';
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case CREATE_EVENT:
+      return Object.assign({ guests:[] }, action.payload.data);
+
+    default:
+      return state;
+  }
+}
