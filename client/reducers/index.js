469e73c4cad87ca093005dc2d739bef75feda60e
davidlewallen <lewallen.david@gmail.com>
2016-08-03 15:32:05 -0500

    (feat) Add hiddenEvent reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,6 +3,7 @@ import CurrentUserReducer from './reducer_currentUser';
 import CurrentEventReducer from './reducer_eventDetails';
 import EventListReducer from './reducer_eventList';
 import EventCreateReducer from './reducer_eventCreate';
+import HiddenEventReducer from './reducer_hiddenEvents'
 
 // So in the regular code, your component created an action which went to actions.js.  That then goes to a specific reducer.  All the reducers are then bundled up here
 // and sent to the store.
@@ -10,7 +11,8 @@ const rootReducer = combineReducers({
   currentUser: CurrentUserReducer,
   currentEvent: CurrentEventReducer,
   eventList: EventListReducer,
-  eventCreate: EventCreateReducer
+  eventCreate: EventCreateReducer,
+  hiddenEvent: HiddenEventReducer
 });
 
 export default rootReducer;
