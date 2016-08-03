d0c62964ae166d64684213fa93978453d82bf937
davidlewallen <lewallen.david@gmail.com>
2016-08-02 19:47:49 -0500

    (feat) User can now leave event

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -11,6 +11,7 @@ export const SET_CURRENT_EVENT = 'SET_CURRENT_EVENT';
 export const USER_LOGOUT = 'USER_LOGOUT';
 export const JOIN_EVENT = 'JOIN_EVENT';
 export const HIDE_EVENT = 'HIDE_EVENT';
+export const LEAVE_EVENT = 'LEAVE_EVENT'
 
 export function fetchCurrentUser() {
   const request = axios.get('/api/auth/loggedIn')
@@ -85,6 +86,16 @@ export function hideEvent(eventId, userId) {
 
   return {
     type: HIDE_EVENT,
+    payload: request
+  }
+}
+
+export function leaveEvent(eventId, userId) {
+  const url = `/api/events/${eventId}/guests/${userId}`
+  const request = axios.delete(url)
+
+  return {
+    type: LEAVE_EVENT,
     payload: request
   }
 }
