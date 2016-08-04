b2c8f32d561eb64a76209ea6c95038de77239d5a
sxcw <yuan329419@gmail.com>
2016-08-03 23:18:15 -0500

    (test) Add title validation

--- client/components/Create.js
+++ client/components/Create.js
@@ -3,193 +3,257 @@ import { Link, browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { create, setCurrentEvent } from '../actions/actions';
+import { createEvent, setCurrentEvent, validateEventForm, updateEventField, clearFormValues, updateTime } from '../actions/actions';
 
-export class Create extends Component {
+export class EventCreate extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      title: "", 
-      description: "", 
-      location: "", 
-      time: "", 
-      duration: "", 
-      max_guests: "", 
-      privacy: "false", 
-      group_visibility: "1",
-      currentUser: this.props.currentUser.id
-    }  
   }
 
-  onKeyPress(event) {
-    this.setState({[event.target.name]:event.target.value})
-  }
-
-  onPrivacyChange(event) {
-    this.setState({privacy:event.target.value})
-  }
-
-  onVisibilityChange(event) {
-    this.setState({group_visibility: event.target.value})
-  }
-
-  validate(event) {
-
-  }
-
-  onSubmit(event) {
+  onSubmitRedux(event) {
     event.preventDefault();
-    this.props.create(this.state)
-      .then(res => {
-        if (res.error) throw new Error('Unable to create event');
-        return this.props.setCurrentEvent(this.props.newEvent);
-      })
-      .then(() => { browserHistory.push(`/${this.props.newEvent.id}`) })
-      .catch(err => {
-        // clear form and display error?
-        console.log(err)
-      });
+    this.props.validateEventForm(this.props.eventFormData);
+    const self = this;
+    this.props.createEvent(this.props.currentUser, (resp) => {
+      if (resp.error) {
+        self.setState({error_location: 'Location invalid'});
+        console.log('error!!!')
+        throw new Error('Unable to create event');
+      }; 
+      browserHistory.push(`/${self.props.newEventId}`);
+    });
+  }
+
+  onFieldChangeRedux(event) {
+    console.log(`onfieldchange: ${event.target.name} : ${event.target.value}`)
+    this.props.updateEventField(event.target.name, event.target.value);
   }
 
   onClearValues(event) {
-    this.setState({
-      title: "", 
-      description: "", 
-      location: "", 
-      time: "", 
-      duration: "", 
-      max_guests: "", 
-      privacy: "false", 
-      group_visibility: "1"
-    })
+    this.props.clearFormValues();
   }
 
   render() {
+
+    const { validationErrors, eventFormData } = this.props;
     return(
       <div className="container">
-          <br/>
-          <h3 className="row">Create New Event</h3>
-          <br/>
-          <form role="form">
-            <div className="form-group row">  
-              <label className="col-xs-4">Title</label>     
-              <input 
-                className="col-xs-8"
-                type="text" 
-                name="title" 
-                placeholder="Event title" 
-                value={this.state.title} 
-                onChange={this.onKeyPress.bind(this)}/>
+        <br/>
+        <h3 className="row">Create New Event</h3>
+        <p>* required</p>
+        <br/>
+        <form role="form">
+          <div className="form-group row">  
+            <label className="col-xs-4">Title*</label>     
+            <input 
+              className="col-xs-8"
+              type="text" 
+              name="title" 
+              placeholder="Event title" 
+              value={eventFormData.title}
+              onBlur={this.onFieldChangeRedux.bind(this)}
+              onChange={this.onFieldChangeRedux.bind(this)}/>
+              {validationErrors.title ? <div className="text-danger"> {validationErrors.title} </div> : null}
+          </div>
+          <div className="form-group row">
+            <label className="col-xs-4">Description*</label>
+            <input 
+              className="col-xs-8"
+              type="text" 
+              name="description" 
+              placeholder="Description" 
+              value={eventFormData.description}
+              onBlur={this.onFieldChangeRedux.bind(this)}
+              onChange={this.onFieldChangeRedux.bind(this)}/>
+              {validationErrors.description ? <div className="text-danger"> {validationErrors.description} </div> : null}
+          </div>
+          <div className="form-group row">
+            <label className="col-xs-4">Location*</label>
+            <input 
+              className="col-xs-8"
+              type="text" 
+              name="location" 
+              placeholder="Location" 
+              value={eventFormData.location}
+              onBlur={this.onFieldChangeRedux.bind(this)}
+              onChange={this.onFieldChangeRedux.bind(this)}/>
+              {validationErrors.location ? <div className="text-danger"> {validationErrors.location} </div> : null}
+          </div>
+          <div className="form-group row">
+            <label className="col-xs-4">Time*</label>
+            <div className="col-xs-2">
+              <select name="hour"  
+                className="form-control"               
+                value={eventFormData.hour}
+                onBlur={this.onFieldChangeRedux.bind(this)}
+                onChange={this.onFieldChangeRedux.bind(this)}>
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((h) => {
+                  return (
+                    <option key={h} value={h}>{h}</option>
+                  );
+                })}
+              </select>
             </div>
-            <div className="form-group row">
-              <label className="col-xs-4">Description</label>
-              <input 
-                className="col-xs-8"
-                type="text" 
-                name="description" 
-                placeholder="Description" 
-                value={this.state.description} 
-                onChange={this.onKeyPress.bind(this)}/>
+            <div className="col-xs-2">
+              <select name="minute" 
+                className="form-control"   
+                value={10 * Math.ceil(eventFormData.minute/10)}
+                onBlur={this.onFieldChangeRedux.bind(this)}
+                onChange={this.onFieldChangeRedux.bind(this)}>
+                {['00', '10', '20', '30', '40', '50'].map((minute) => {
+                  return (
+                    <option key={minute} value={minute}>{minute}</option>
+                  );
+                })}
+              </select>
             </div>
-            <div className="form-group row">
-              <label className="col-xs-4">Location</label>
-              <input 
-                className="col-xs-8"
-                type="text" 
-                name="location" 
-                placeholder="Location" 
-                value={this.state.location} 
-                onChange = {this.onKeyPress.bind(this)}/>
+            <div className="col-xs-2">
+              <select name="ampm"
+                className="form-control"
+                value={eventFormData.ampm}
+                onBlur={this.onFieldChangeRedux.bind(this)}
+                onChange={this.onFieldChangeRedux.bind(this)}>
+                <option value="am">am</option>
+                <option value="pm">pm</option>
+              </select>
             </div>
-            <div className="form-group row">
-              <label className="col-xs-4">Time</label>
-              <input 
-                className="col-xs-8"
-                name="time" 
-                type="datetime-local" 
-                placeholder="Date" 
-                value={this.state.time} 
-                onChange={this.onKeyPress.bind(this)}/>
+            <div className="col-xs-2">
+              <select name="is_tomorrow"
+                className="form-control"
+                value={eventFormData.is_tomorrow}
+                onBlur={this.onFieldChangeRedux.bind(this)}
+                onChange={this.onFieldChangeRedux.bind(this)}>
+                <option value="false">today</option>
+                <option value="true">tomorrow</option>
+              </select>
+              {validationErrors._time ? <div className="text-danger"> {validationErrors._time} </div> : null}
             </div>
-            <div className="form-group row">
-              <label className="col-xs-4 ">Duration</label>
-              <input 
-                className="col-xs-8"
-                name="duration" 
-                type="number" 
-                placeholder="Duration" 
-                value={this.state.duration} 
-                onChange={this.onKeyPress.bind(this)}/>
+            <div className="row">
+              <div className="col-xs-4"></div>
+              <div className="col-xs-2 text-info"> hour </div>
+              <div className="col-xs-2 text-info"> minute </div>
             </div>
-            <div className="form-group row">
-              <label className="col-xs-4">Guests</label>
+            {/*<div className="col-xs-8">
               <input 
-                className="col-xs-8"
-                name="max_guests" 
-                type="number" 
-                placeholder="Max number of guests" 
-                value={this.state.max_guests} 
-                onChange={this.onKeyPress.bind(this)}/>
+                type="checkbox" 
+                className="" 
+                role="button"
+                onClick={this.setCurrentTime.bind(this)}/> 
+                <span> Event happening now? </span>
+             
+            </div>*/}
+            <div className="row">
+            Event will start
             </div>
-            <br/>
-            <div className="form-group row">
-              <label className="col-xs-4">Privacy</label>
-              <div>
-                <label className="col-xs-2">
-                  <input 
-                    className="form-check-input"
-                    name="privacy" 
-                    type="radio" 
-                    value="false" 
-                    checked={this.state.privacy === "false"} 
-                    onChange={this.onPrivacyChange.bind(this)}/> 
-                  <span>public</span>
-                </label>
-                <label className="col-xs-2">
-                  <input 
-                    className="form-check-input"
-                    name="privacy" 
-                    type="radio" 
-                    value="true" 
-                    checked={this.state.privacy === "true"} 
-                    onChange={this.onPrivacyChange.bind(this)}/> 
-                    <span>private</span>
-                </label>
+            <div className="row">
+              <div className="col-xs-4"></div>
+              <div className="col-xs-8"> 
+                {validationErrors.hour ? <div className="text-danger"> {validationErrors.hour} </div> : null}
+                {validationErrors.minute ? <div className="text-danger"> {validationErrors.minute} </div> : null}
+                {validationErrors.ampm ? <div className="text-danger"> {validationErrors.ampm} </div> : null}
               </div>
             </div>
-            <div className="form-group row">
-              <label className="col-xs-4">Visibility</label>
-              <div>
-                <label className="col-xs-2">
-                  <input 
-                    className="form-check-input"
-                    name="group_visibility" 
-                    type="radio" 
-                    value="1" 
-                    checked={this.state.group_visibility === "1"} 
-                    onChange={this.onVisibilityChange.bind(this)}/> 
-                    <span>group1</span>
-                  </label>
-              </div>
+          </div>
+
+          <div className="form-group row">
+            <label className="col-xs-4">Duration</label>
+            <div className="col-xs-2">
+              <select name="duration_hour"  
+                className="form-control"               
+                value={eventFormData.duration_hour}
+                onBlur={this.onFieldChangeRedux.bind(this)}
+                onChange={this.onFieldChangeRedux.bind(this)}>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((h) => {
+                  return (
+                    <option key={h} value={h}>{h}</option>
+                  );
+                })}
+              </select>                
             </div>
-            <div className="btn-toolbar">
-              <button 
-                type="submit" 
-                className='btn btn-primary' 
-                role="button"
-                onClick={this.onSubmit.bind(this)}> 
-                Submit 
-              </button>
-              <button 
-                type="button" 
-                className='btn btn-danger' 
-                role="button"
-                onClick={this.onClearValues.bind(this)}> 
-                Clear Values 
-              </button>
+            <div className="col-xs-2">
+              <select name="duration_minute" 
+                className="form-control"   
+                value={10 * Math.ceil(eventFormData.duration_minute/10)}
+                onBlur={this.onFieldChangeRedux.bind(this)}
+                onChange={this.onFieldChangeRedux.bind(this)}>
+                {['00', '10', '20', '30', '40', '50'].map((minute) => {
+                  return (
+                    <option key={minute} value={minute}>{minute}</option>
+                  );
+                })}
+              </select>
             </div>
-          </form>
+          </div>
+          <div className="form-group row">
+            <label className="col-xs-4">Guests</label>
+            <input 
+              className="col-xs-4"
+              name="max_guests" 
+              type="number" 
+              placeholder="Number of guests" 
+              value={eventFormData.guests} 
+              onChange={this.onFieldChangeRedux.bind(this)}/>
+          </div>
+          <br/>
+          <div className="form-group row">
+            <label className="col-xs-4">Privacy</label>
+            <div>
+              <label className="col-xs-2">
+                <input 
+                  className="form-check-input"
+                  name="privacy" 
+                  type="radio" 
+                  value="public"
+                  checked={eventFormData.privacy === 'public'} 
+                  onChange={this.onFieldChangeRedux.bind(this)}/> 
+                <span>public</span>
+              </label>
+              <label className="col-xs-2">
+                <input 
+                  className="form-check-input"
+                  name="privacy" 
+                  type="radio" 
+                  value="private"
+                  checked={eventFormData.privacy === 'private'} 
+                  onChange={this.onFieldChangeRedux.bind(this)}/> 
+                  <span>private</span>
+              </label>
+            </div>
+          </div>
+          <div className="form-group row">
+            <label className="col-xs-4">Visibility</label>
+            <div>
+              <label className="col-xs-2">
+                <input 
+                  className="form-check-input"
+                  name="group_visibility" 
+                  type="radio" 
+                  value="1" 
+                  checked={eventFormData.group_visibility} 
+                  onChange={this.onFieldChangeRedux.bind(this)}/> 
+                  <span>group1</span>
+                </label>
+            </div>
+          </div>
+          <div className="btn-toolbar">
+            <button 
+              type="submit" 
+              className='btn btn-primary' 
+              role="button"
+              onClick={this.onSubmitRedux.bind(this)}> 
+              Submit 
+            </button>
+            <button 
+              type="button" 
+              className='btn btn-danger' 
+              role="button"
+              onClick={this.onClearValues.bind(this)}> 
+              Clear Values 
+            </button>
+          </div>
+        </form>
+        {validationErrors._form ? <div className="text-danger"> {validationErrors._form} </div> : null}
       </div>
     )
   }  
@@ -197,16 +261,16 @@ export class Create extends Component {
 
 function mapStateToProps(state) {
   return {
-    newEvent: state.create,
-    currentUser: state.currentUser
+    newEvent: state.eventCreate,
+    currentUser: state.currentUser,
+    validationErrors: state.eventCreate.validationErrors,
+    eventFormData: state.eventCreate.eventFormData
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({
-    create, 
-    setCurrentEvent
-  }, dispatch)
+    createEvent, setCurrentEvent, validateEventForm, updateEventField, clearFormValues, updateTime}, dispatch)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Create);
+export default connect(mapStateToProps, mapDispatchToProps)(EventCreate);
