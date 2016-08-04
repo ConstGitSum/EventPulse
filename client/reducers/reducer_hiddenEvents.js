37dbf0dd469023a814dc28a7a208e11206158cf0
davidlewallen <lewallen.david@gmail.com>
2016-08-04 15:20:30 -0500

    (feat) Returns an array of hidden events to be filtered

--- client/reducers/reducer_hiddenEvents.js
+++ client/reducers/reducer_hiddenEvents.js
@@ -1,10 +1,15 @@
-import { HIDE_EVENT } from '../actions/actions';
+import { GET_HIDDEN_EVENTS, HIDE_EVENT } from '../actions/actions';
 
 export default function(state = [], action) {
   switch (action.type) {
+    case GET_HIDDEN_EVENTS:
+      let hiddenEvents = []
+      action.payload.data.forEach((event) => hiddenEvents.push(event.event_id))
+
+      return hiddenEvents
     case HIDE_EVENT:
       return [...state, action.payload.data.event_id]; 
-    default: 
+    default:
       return state;
   }
 }
