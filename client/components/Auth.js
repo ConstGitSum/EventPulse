036792842addcbb56a9f032a36d342792ed3df3a
Tony Kung <lps.tony@gmail.com>
2016-08-04 15:14:51 -0500

    styling Auth page

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -4,12 +4,16 @@ import React, { Component } from 'react';
 export default class Auth extends Component {
   render() {
     return (
-      <div>
-        <a 
-          href='/api/passportFacebook/facebookLogin' 
-          className="btn btn-primary"> 
-            Login with Facebook
-        </a>
+      <div className="pure-g">
+        <div className="pure-u-1">
+          <h1 className="auth-head">PULSE</h1>
+            <a 
+              href='/api/passportFacebook/facebookLogin' 
+              className="pure-button pure-button-primary"> 
+               <i className="fa fa-facebook-official" aria-hidden="true"></i> LOGIN!  
+            </a>
+          
+        </div>
       </div>
     );
   }
