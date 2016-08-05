f8d9f2a275f162afb07ac4ef5fd232ef8b5a4596
davidlewallen <lewallen.david@gmail.com>
2016-08-05 00:02:55 -0500

    (refactor) to allow hidden events to be delete from hidden_events table

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -175,10 +175,8 @@ router.post('/:id/hide', function(req, res, next) {
 });
 
 // *** DELETE new hidden event *** //
-router.delete('/:id/hide', function(req, res, next) {
-  console.log("eventId: ", req.params.id)
-  console.log("userId: ", req.body.user_id)
-  Hide.unhide(req.params.id, req.body.user_id)
+router.delete('/:id/hide/:user_id', function(req, res, next) {
+  Hide.unhide(req.params.id, req.params.user_id)
     .then(() => {
       res.status(200).json({ status: 'deleted' });
     })
