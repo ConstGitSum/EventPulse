6a2ea3da361d719594bccf7eccf9aae9499a4760
davidlewallen <lewallen.david@gmail.com>
2016-08-05 21:37:26 -0500

    (refactor) Styles are now in Sidebar file and code cleanup

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -3,16 +3,54 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router';
 
-var Menu = require('react-burger-menu').slide
+let Menu = require('react-burger-menu').slide
+
+let styles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    left: '97%',
+    top: '2%'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenu: {
+    background: '#373a47',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0)'
+  }
+}
+
 
 export default class Sidebar extends React.Component {
   render() {
-    {console.log("side")}
     return (
-      <Menu right>
-        <a id="home" 
-          className="menu-item" 
-          onClick={console.log("clicked1")}>1</a>
+      <Menu right styles={ styles}>
+        <span>Power</span>
+        <span>User</span>
+        <span>Things</span>
+        <span>And</span>
+        <span>Stuff</span>
+        <span>Edit</span>
       </Menu>
     )
   }
