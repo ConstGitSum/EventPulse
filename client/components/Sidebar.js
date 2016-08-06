31260138d9432567ec336d737fd756457faea2a6
davidlewallen <lewallen.david@gmail.com>
2016-08-06 14:13:36 -0500

    (refactor) Code cleanup

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -9,42 +9,6 @@ import { hideEvent, unhideEvent } from '../actions/actions';
 
 let Menu = require('react-burger-menu').slide;
 
-let styles = {
-  bmBurgerButton: {
-    position: 'fixed',
-    width: '36px',
-    height: '30px',
-    left: '97%',
-    top: '2%'
-  },
-  bmBurgerBars: {
-    background: '#373a47'
-  },
-  bmCrossButton: {
-    height: '24px',
-    width: '24px'
-  },
-  bmCross: {
-    background: '#bdc3c7'
-  },
-  bmMenu: {
-    background: '#373a47',
-    padding: '2.5em 1.5em 0',
-    fontSize: '1.15em'
-  },
-  bmMorphShape: {
-    fill: '#373a47',
-  },
-  bmItemList: {
-    color: '#b8b7ad',
-    padding: '0.8em'
-  },
-  bmOverlay: {
-    background: 'rgba(0, 0, 0, 0)'
-  }
-}
-
-
 export class Sidebar extends React.Component {
   onClickGuests() {
   }
