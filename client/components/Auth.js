aa02d1ef1b155287326b3b76bf5f0afc82efeef5
Tony Kung <lps.tony@gmail.com>
2016-08-11 19:08:42 -0500

    (feat)add background map image for landing page

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -7,7 +7,9 @@ export default class Auth extends Component {
       <div className="container" className="pure-g">
         <div className="row">
           <div className="col-md-12" className="pure-u-1">
-            <h1 className="page-header" className="auth-head">PULSE</h1>
+          <div className="col-xs-12" className="background_image">
+          </div>
+            <h1 className="col-xs-12" className="page-header" className="auth-head">PULSE</h1>
               <a 
                 href='/api/passportFacebook/facebookLogin' 
                 className="pure-button pure-button-primary"
