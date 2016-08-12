a6451a57381e020705ef2e45cd43a420fd95569c
davidlewallen <lewallen.david@gmail.com>
2016-08-12 14:43:18 -0500

    (feat) Centered landing page

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -4,20 +4,22 @@ import React, { Component } from 'react';
 export default class Auth extends Component {
   render() {
     return (
+
       <div className="container" className="pure-g">
         <div className="row">
           <div className="col-md-12" className="pure-u-1">
-          <div className="col-xs-12" className="background_image">
+            <div className="col-xs-12" className="background_image">
+            </div>
+            <div className="landing-page-title">
+              <h1 className="col-xs-12" className="page-header" className="auth-head">PULSE</h1>
+              <a
+                href='/api/passportFacebook/facebookLogin'
+                className="pure-button pure-button-primary">
+              <span className="fa fa-facebook-official" aria-hidden="true"></span>
+                LOGIN!
+              </a>
+            </div>
           </div>
-            <h1 className="col-xs-12" className="page-header" className="auth-head">PULSE</h1>
-              <a 
-                href='/api/passportFacebook/facebookLogin' 
-                className="pure-button pure-button-primary"
-                > 
-                <span className="fa fa-facebook-official" aria-hidden="true"></span>
-                LOGIN!  
-              </a> 
-          </div>    
         </div>
       </div>
     );
