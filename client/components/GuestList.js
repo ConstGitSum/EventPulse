b390bbc776054e8a44da0e13be014ea32ff7bd93
Jad Carson <jad.carson@gmail.com>
2016-08-15 12:15:17 -0500

    Feat161 (#173)
    * (feature) Guest list now shows who is going.  When you click, it shows you friends to invite
    
    * (feature) working on guest list
    
    * (feat) guest list/ friends list changing working better
    
    * (refactor) changed sizes of words
    
    * (refactor) added double quotes

---
+++ client/components/GuestList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+//import io from 'socket.io-client'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import axios from 'axios'
+import {addInvite} from '../actions/actions'
+import { CardStack, Card } from 'react-cardstack';
+import FriendsList from './FriendsList'
+
+export class GuestList extends React.Component {
+  
+  render() {
+    return(
+      <div>
+      <ul className = "guests">{this.props.currentEvent.guests.map((guest) => {
+       return <li key = {guest.id} className = "guest"><img src  = {guest.image} className = "eventListFriendImage"/> {guest.name} </li>
+      })} </ul>
+      </div>
+      )
+  }
+
+  }
+
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser,
+    currentEvent: state.currentEvent
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({addInvite},dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GuestList);
