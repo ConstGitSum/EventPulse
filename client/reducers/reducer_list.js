dff86f542e4be9b2bd82ca43f08aa9f863cd3b5a
Jad Carson <Jad.Carson@gmail.com>
2016-08-15 16:43:48 -0500

    fixed production seeds

--- client/reducers/reducer_list.js
+++ client/reducers/reducer_list.js
@@ -1,7 +1,6 @@
 import { GET_EVENTS } from '../actions/actions';
 
 export default function(state = [], action) {
-  console.log("ACTION", action.payload)
   switch (action.type) {
     case GET_EVENTS: {
       return action.payload;
