4711cb99b9a741dca45cc742ceb0ed9e5c87ae8e
Kenneth Torng <ktorng@gmail.com>
2016-08-17 10:34:55 -0500

    (refactor) Use https

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -8,11 +8,11 @@ export default class Auth extends Component {
         <div className="background_image"></div>
         <div className="hero-container">
           <h1 className="page-header" className="auth-head">PULSE</h1>
-          <a
-            href='/api/passportFacebook/facebookLogin'
-            className="pure-button pure-button-primary login-button">
-          <span className="fa fa-facebook-official facebook-icon" aria-hidden="true"></span>
-            SIGN IN
+          <a href='/api/passportFacebook/facebookLogin'>
+            <button className="btn btn-large login-button">
+              <span className="fa fa-facebook-official facebook-icon" aria-hidden="true"></span>
+              SIGN IN
+            </button>
           </a>
         </div>
       </div>
--- server/passport.js
+++ server/passport.js
@@ -1,11 +1,11 @@
 var FacebookStrategy = require('passport-facebook').Strategy;
 const FACEBOOK_ID = process.env.FACEBOOK_ID;
 const FACEBOOK_SECRET = process.env.FACEBOOK_SECRET;
-const FACEBOOK_CALLBACK_URL = process.env.NODE_ENV === 'production' 
-  ? 'http://eventpulse.herokuapp.com/api/passportFacebook/facebookLogin/Callback' 
+const FACEBOOK_CALLBACK_URL = process.env.NODE_ENV === 'production'
+  ? 'https://eventpulse.herokuapp.com/api/passportFacebook/facebookLogin/Callback'
   : 'http://localhost:3000/api/passportFacebook/facebookLogin/Callback'
 
-const PROFILE_FIELDS = ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'picture.type(small)', 'verified', 'friends'] //This is what tells facebook what to return		
+const PROFILE_FIELDS = ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'picture.type(small)', 'verified', 'friends'] //This is what tells facebook what to return
 
 var User = require('./models/user');
 var PassportHelper = require('./passport_helper')
