3e60cb430ffd8215fd2649393b4811b02308a674
davidlewallen <lewallen.david@gmail.com>
2016-08-21 10:25:22 -0500

    (refactor) Linted GuestList.jsx

--- client/components/GuestList.jsx
+++ client/components/GuestList.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-// import io from 'socket.io-client'
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import axios from 'axios';
 import { addInvite } from '../actions/actions';
-import { CardStack, Card } from 'react-cardstack';
-import FriendsList from './FriendsList';
 
 export class GuestList extends React.Component {
-
   render() {
     return (
       <div>
-      <ul className="totalGuests">{this.props.currentEvent.guests.map((guest) => {
-        return <li key={guest.id} className="event-item list-group-item guest"><img src={guest.image} className="largePicture eventListFriendImage" /> {guest.name} </li>;
-      })} </ul>
+        <ul className="totalGuests">{this.props.currentEvent.guests.map((guest) =>
+          <li key={guest.id} className="event-item list-group-item guest">
+            <img
+              src={guest.image}
+              className="largePicture eventListFriendImage"
+              role="presentation"
+            />
+            {guest.name}
+          </li>
+        )}</ul>
       </div>
-      );
+    );
   }
+}
 
-  }
+GuestList.propTypes = {
+  currentEvent: PropTypes.object.isRequired,
+};
 
 /* istanbul ignore next */
 function mapStateToProps(state) {
